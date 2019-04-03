b88182a2e79993c528ed3cc12153aff5e465a8af
spence <johnson.s.8253@gmail.com>
2019-04-03 16:59:21 -0600

    added group project pics

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
-var slideIndex = 0;
+let slideIndex = 0;
 carousel();
 
 function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
+  let x = document.getElementsByClassName("mySlides");
+  for (let i = 0; i < x.length; i++) {
     x[i].style.display = "none"; 
   }
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1} 
   x[slideIndex-1].style.display = "block"; 
-  setTimeout(carousel, 3000); // Change image every 2 seconds
+  setTimeout(carousel, 3000); // Change image every 3 seconds
+}
+
+let slideIndex2 = 0;
+carousel2();
+
+function carousel2() {
+  let x = document.getElementsByClassName("mySlides2");
+  for (let i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
+  }
+  slideIndex2++;
+  if (slideIndex2 > x.length) {slideIndex2 = 1} 
+  x[slideIndex2-1].style.display = "block"; 
+  setTimeout(carousel2, 3000); // Change image every 3 seconds
 }
